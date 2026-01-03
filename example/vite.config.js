b1de538c694d7058078250b2cb7848bd79763aca
一丝 <percyley@qq.com>
2026-01-03 16:41:04 +0800

    feat: simplify the code and use Vite to launch the video demo page.

---
+++ example/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    https: true,
+    open: '/video.html'
+  },
+  plugins: [mkcert()]
+})
